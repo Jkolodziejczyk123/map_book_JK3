0a83e2edb924399ddf5975dbedbf34176337eb64
Jkolodziejczyk123 <167193640+Jkolodziejczyk123@users.noreply.github.com>
2024-05-28 19:52:16 +0200

    wip: add user printing functionlity

---
+++ main.py
@@ -0,0 +1,20 @@
+name="Dominik"
+surname="Kuźnik"
+post=5
+
+print (f"Twój znajomy {name} {surname} opublikował {post} postów")
+
+name_2="Jakub"
+surname_2="Kołodziejczyk"
+post_2=8
+
+print (f"Twój znajomy {name_2} {surname_2} opublikował {post_2} postów")
+
+name_3="Kacper"
+surname_3="Macioch"
+post_3=10
+
+print (f"Twój znajomy {name_3} {surname_3} opublikował {post_3} postów")
+
+
+
